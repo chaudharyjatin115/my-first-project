1fe587b1829483d63b073640fd958fbcfecaf9b1
Jatin <Chaudharyjatin115@gmail.com>
2021-02-18 18:48:26 +0530

    a madlibs in pyton
    second project

---
+++ madlibs game in python.py
@@ -0,0 +1,6 @@
+color= input('Enter a color: ')
+plural_nouns= input("Enter a plural noun: ")
+celebrity= input("Enter a celebrity: ")
+print('Roses are' + color)
+print(plural_nouns + "are blue ")
+print(celebrity + "loves you")
