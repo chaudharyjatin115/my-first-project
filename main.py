ff622b430042c32974300588af630ae1248e6b1d
Jatin <Chaudharyjatin115@gmail.com>
2021-02-23 20:01:49 +0530

    this is a mcq question app in python
    it asks question to user and checks them against the saved questions

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-num1 = (" Enter a number: ")
-num2 = ("Enter another number: ")
-result =str(float(num1)) + str(float(num2))
-print(result)
+from questions import Question
+question_prompts = [
+    " Who is the PM of India\n (a) MODI\n (b) Rahul\n (c) Akhilesh\n\n",
+    "who is pres of us\n (a) trump\n (b)harsh (c) biden\n\n",
+    "who is pm of pak\n ()imran\n (b)irfan\n (c) ritik\n\n"
+]
+
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "a"),
+    Question(question_prompts[2], "a")
+
+
+
+
+]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print(" You got " + str(score) + "/" + str(len(questions)) + " correct ")
+run_test(questions)
