7af6db734d984760edffe19e5c9ad338b6d12819
Jatin <Chaudharyjatin115@gmail.com>
2021-03-05 23:29:30 +0530

    rockpaperscissors

--- main.py
+++ main.py
@@ -1,25 +1,18 @@
-from questions import Question
-question_prompts = [
-    " Who is the PM of India\n (a) MODI\n (b) Rahul\n (c) Akhilesh\n\n",
-    "who is pres of us\n (a) trump\n (b)harsh (c) biden\n\n",
-    "who is pm of pak\n ()imran\n (b)irfan\n (c) ritik\n\n"
-]
+ randoimportm
+def play():
+    user = input("enter your choice'r' for rock,'p' for paper, 's' for scissors\n")
+    computer = random.choice(['r','p','s'])
 
-questions = [
-    Question(question_prompts[0], "a"),
-    Question(question_prompts[1], "a"),
-    Question(question_prompts[2], "a")
+    if user == computer:
+        return 'its a tie'
 
+    if is_win(user , computer):
+        return 'you win'
 
+    return'you lost'
 
-
-]
-
-def run_test(questions):
-    score = 0
-    for question in questions:
-        answer = input(question.prompt)
-        if answer == question.answer:
-            score += 1
-    print(" You got " + str(score) + "/" + str(len(questions)) + " correct ")
-run_test(questions)
+def is_win(player,opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+        or(player == 'p' and opponent == 'r'):
+        return True
+print(play())
